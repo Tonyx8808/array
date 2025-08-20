f5088faf327397ce0a91e3f90c5b28b359d74c23
Antonio Russo <antoniorusso1988@outlook.it>
2025-08-20 15:51:59 +0200

    2° ESERCIZIO

--- index.js
+++ index.js
@@ -2,22 +2,48 @@
 //// Exercise: Given an array of products, filter only the products that are in stock
 // and return an array with their names in uppercase
 
-const products = [
+/*const products = [
   { id: 1, name: 'Laptop', price: 999, inStock: true },
   { id: 2, name: 'Mouse', price: 25, inStock: false },
   { id: 3, name: 'Keyboard', price: 75, inStock: true },
   { id: 4, name: 'Monitor', price: 350, inStock: false },
   { id: 5, name: 'Headphones', price: 85, inStock: true },
-];
+];*/
 
 /*filtro la condizione dei device disponibili con product.filter (serve a creare un nuovo array che contengono solo gli elementi
-che soddisfano una condizione specifica in questoc aso true ------- mentre product.inStock è una 
+che soddisfano una condizione specifica in questo caso true ------- mentre product.inStock è una 
 proprietà di ogni ogetto del prodotto che può avere come valore true o false)
 in questo caso è true perchè si è filtrato solamente i prodotti disponibili*/
 
-const availableDevice = products.filter(product => product.inStock);
-console.log(availableDevice);
-/* se voglio filtrare solamente i prodotti non disponibili creo la stessa sintassi ma aggiungo l'operatore logico NOT(!)
+//const availableDevice = products.filter(products => products.inStock);
+//console.log(availableDevice);//
 
-const notavaibleDevice = product.filter(product => !product.inStock);
-console.log(notavaibleDevice); qui mi stampa nel console.log i prodotti non disponibili*/
+// se voglio filtrare solamente i prodotti non disponibili creo la stessa sintassi ma aggiungo l'operatore logico NOT(!)//
+
+/*const notavaibleDevice = products.filter(product => !product.inStock);
+console.log(notavaibleDevice); //qui mi stampa nel console.log i prodotti non disponibili*/
+
+//2° ESERCIZIO
+
+// Exercise: Given an array of products, filter only the products that are in stock
+// considering quantity and applying a 10% discount if total is over 100
+
+const shoppingCart = [
+  { item: 'Book', price: 15, quantity: 2 },
+  { item: 'Pen', price: 2, quantity: 10 },
+  { item: 'Notebook', price: 8, quantity: 3 },
+  { item: 'Backpack', price: 45, quantity: 1 },
+];
+
+const totalCart = shoppingCart.reduce((acc, products) =>{
+  return acc + products.price * products.quantity
+}, 0);
+
+let finalPrice = totalCart;
+if(totalCart > 100){
+  finalPrice = totalCart -(0.1 * totalCart);
+}
+
+console.log("totale", totalCart);
+console.log("prezzo finale", finalPrice);
+
